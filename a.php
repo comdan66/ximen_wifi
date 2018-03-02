4cdf6c184a9a1f29a05f99713923e81e31cc5850
OA Wu <comdan66@gmail.com>
2018-03-02 17:36:18 +0800

    Test

--- a.php
+++ a.php
@@ -1,17 +1,8 @@
-<form action="http://125.227.37.68/auth/index.html/u" method="post">
-
+<form id="frm1" action="http://125.227.37.68/auth/index.html/u" method="post">
 <input type="hidden" name="tag" value=""/>
 <input type="hidden" name="gid" value=""/>
 <input type="hidden" name="g_address" value=""/>
 <input type="hidden" name="email" value="nodata@demo.com"/>
 <input type="hidden" name="cmd" value="authenticate"/>
-
-<div id="start-bg"></div>
-<div id="start-img">
-<input id="start-img-btn"
-type="image"
-src="/assets/img/6efaa703b4a04d1b86f3b2e4f44396ae.png"
-alt="Submit Form"
-onclick="window.open('/b.php','_blank');this.form.submit();"/>
-</div>
+<button>點我</button>
 </form>
